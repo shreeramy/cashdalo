455ad9f6638676585a7858635db908d0f8ea6faf
shreeramy <104049334+shreeramy@users.noreply.github.com>
2022-06-20 23:08:29 +0530

    Update settings.py

--- cash_ledger/settings.py
+++ cash_ledger/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-5+&ls0fjv9hb32hb*o)!v@w2uqs5#h&pvrm@c=js#k)nt&$1&v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
